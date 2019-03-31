127450ee74639a643c32b287b2c02e3337069d72
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2019-03-31 19:26:55 -0300

    Arregla definición de datatypes . Agrega funcion cantidad tesoros.

--- piratas_caribe.hs
+++ piratas_caribe.hs
@@ -1,9 +1,25 @@
 data Pirata = Pirata {nombrePirata :: String,
-                     botin :: Tesoro
+                     botin :: [Tesoro]
                      } deriving (Show)
 
 data Tesoro = Tesoro {nombreTesoro :: String,
                      valor :: Integer
                      } deriving (Show)
 
+juventud :: Tesoro
+juventud = Tesoro { nombreTesoro = "Juventud",
+                    valor = 20
+                  }
 
+belleza :: Tesoro
+belleza = Tesoro {nombreTesoro = "Belleza",
+                valor = 30
+                }
+
+viotti :: Pirata
+viotti = Pirata { nombrePirata = "Viotti",
+            botin = [juventud, belleza]
+         }
+
+cantidad_tesoros :: Pirata -> Int
+cantidad_tesoros pirata = length (botin pirata)
