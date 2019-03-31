273b4e7f3989b67caae566f2d65dee0a100623b1
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2019-03-31 18:09:15 -0300

    Ajustar coding style, corregir definición de Data Tesoro

--- piratas_caribe.hs
+++ piratas_caribe.hs
@@ -1,7 +1,8 @@
-data Pirata = Pirata {nombre :: String
-                     ,botin :: Tesoro
+data Pirata = Pirata {nombre :: String,
+                     botin :: Tesoro
                      } deriving (Show)
 
-data Tesoro {nombre :: String
-            ,valor :: Integer
-            } deriving (Show)
+data Tesoro = Tesoro{nombre :: String,
+                    valor :: Integer
+                    } deriving (Show)
+
