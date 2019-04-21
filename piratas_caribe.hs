3aa1fd619f1d064027b2928c6b969d00df37324a
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2019-04-20 21:16:24 -0300

    Dev of the game in progress.

--- piratas_caribe.hs
+++ piratas_caribe.hs
@@ -70,7 +70,7 @@ ron :: Tesoro
 ron = Tesoro {nombreTesoro = "Ron", valor = 25}
 
 media_sucia :: Tesoro
-    media_sucia = Tesoro {nombreTesoro = "Media sucia", valor = 10}
+media_sucia = Tesoro {nombreTesoro = "Media sucia", valor = 10}
 
 --PIRATAS
 viotti :: Pirata
@@ -216,7 +216,7 @@ comenzar_historia = do
     putStrLn("Es hora de comenzar tu aventura! Pero antes...")
     protagonista <- crear_pirata
     putStrLn("Ahora es hora de salir a navegar los 7 mares! Tu historia comienza en la isla Tortuga.")
-    resultado_historia <- menu_historia_sin_barco protagonista
+    resultado_historia <- menu_historia protagonista
     return resultado_historia
 
 crear_pirata :: IO (Pirata)
@@ -228,30 +228,64 @@ crear_pirata = do
     putStrLn("... tienes un botin, verdad?")
     putStrLn("Agh... no tienes botin... bueno, entonces tu botin inicial consiste en una media sucia.")
     putStrLn("Si te quedas sin botin, seras expulsado de la vida pirata!")
-    putStrLn("Y recuerda que el coseno del valor de tus tesoros determina tu fortaleza en combate.")
+    putStrLn("Y recuerda que el valor de tus tesoros determina tu fortaleza en combate.")
     return Pirata {nombrePirata = nombreProtagonista, botin = [media_sucia]}
 
-menu_historia_sin_barco :: Pirata -> IO (String)
-menu_historia_sin_barco protagonista = do
+menu_historia :: Pirata -> IO String
+menu_historia protagonista = do
     putStrLn("Que deseas realizar a continuacion?")
     putStrLn("(1)-ROBAR UN BARCO")
-    putStrLn("(2)-SAQUEAR UNA ISLA")
+    putStrLn("(2)-SAQUEAR CIUDAD")
+    putStrLn("(3)-RETIRARSE DE LA PIRATERIA")
+    opcion <- getLine
+    desarrollar_historia (read opcion :: Integer) protagonista 
+
+menu_historia_con_barco :: Barco -> IO String
+menu_historia_con_barco barco = do
+    putStrLn("Que deseas realizar a continuacion?")
+    putStrLn("(1)-ATACAR UN BARCO")
+    putStrLn("(2)-ANCLAR EN UNA ISLA")
     putStrLn("(3)-ATACAR UNA CIUDAD")
     putStrLn("(4)-RETIRARSE DE LA PIRATERIA")
-    desarrollar_historia_sin_barco protagonista
-    
-desarrollar_historia_sin_barco :: Pirata -> Pirata
-desarrollar_historia_sin_barco protagonista
-  | 1 = robar_barco protagonista
-  | 2 = saquear_isla protagonista
-  | 3 = atacar_ciudad protagonista
-  | 4 = retirarse protagonista
-  | otherwise = opcion_incorrecta protagonista 
+    opcion <- getLine
+    desarrollar_historia_en_barco (read opcion :: Integer) barco
 
-robar_barco :: Pirata -> Barco
+desarrollar_historia :: Integer -> Pirata -> IO String 
+desarrollar_historia opcion protagonista = case opcion of
+     1 -> robar_barco protagonista
+--     2 -> saquear_ciudad protagonista
+--     3 -> retirarse protagonista
+     _ -> menu_historia protagonista
+
+desarrollar_historia_en_barco :: Integer -> Barco -> IO String
+desarrollar_historia_en_barco opcion barco = case opcion of
+--  | 1 = atacar_barco protagonista
+--  | 2 = anclar_en_isla barco protagonista
+--  | 3 = retirarse protagonista
+  _ -> desarrollar_historia_en_barco opcion barco
+
+
+-- ACCIONES POSIBLES
+
+robar_barco :: Pirata -> IO String
 robar_barco protagonista = do
     putStrLn("Solo hay un velero triste anclado en el muelle...")
     putStrLn("Pero... que va, lo tomas y zarpas. Enhorabuena! Ahora es tuyo. Que nombre le pondras?")
     nombreVelero <- getLine
-    return Barco { tripulacion = [protagonista] , nombreBarco = nombreVelero}
+    menu_historia_con_barco Barco { tripulacion = [protagonista] , nombreBarco = nombreVelero}
 
+
+
+--- FUNCIONES AUXILIARES
+
+confirmar :: String -> IO Bool
+confirmar mensaje_a_confirmar = do
+    putStrLn (mensaje_a_confirmar)
+    putStrLn ("(s)i/(n)o")
+    confirmacion <- getLine
+    if any (==confirmacion) ["s", "S"] then
+        return True
+    else if any (==confirmacion) ["n", "N"] then
+        return False
+    else 
+        confirmar "Ha ingresado una opcion incorrecta. Por favor, ingrese"
